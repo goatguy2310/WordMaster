507f545a304b01dc8321ddb12e27f3e59511a698
goatguy2310 <duongminhhoang2310@gmail.com>
2024-03-03 16:40:05 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,118 @@
+import WordGrid from "./components/WordGrid";
+import "./App.css";
+import send from "./assets/send.png";
+import erase from "./assets/erase.png";
+import words from "./assets/words_dictionary.json";
+import { useState } from "react";
+
+function getRandomInt(max: number) {
+	return Math.floor(Math.random() * max);
+}
+
+const App = () => {
+	let b = [];
+	for (let i = 0; i < 96; i++) {
+		b.push(String.fromCharCode(65 + getRandomInt(26)));
+	}
+
+	const letterScores = {
+		aeioulnstr: 2,
+		dg: 3,
+		bcmp: 4,
+		fhvwy: 5,
+		k: 8,
+		jx: 10,
+		qz: 15,
+	};
+
+	const [board, setBoard] = useState(b);
+	const [text, setText] = useState("");
+	const [pos, setPos] = useState<number[]>([]);
+
+	const [score, setScore] = useState(0);
+
+	const handleGridClick = (index: number) => {
+		if (board[index] === "") return;
+
+		setPos([...pos, index]);
+
+		setBoard(board.map((item, i) => (i === index ? "" : item)));
+		setText(text + board[index]);
+	};
+
+	const handleEraseClick = () => {
+		if (text === "") return;
+
+		const index = pos[pos.length - 1];
+		setPos(pos.filter((item) => item != index));
+
+		setBoard(
+			board.map((item, i) => (i === index ? text[text.length - 1] : item))
+		);
+		setText(text.slice(0, -1));
+	};
+
+	const handleSubmitClick = () => {
+		if (text.toLowerCase() in words) {
+			setText("");
+
+			let wordScore = 1;
+			for (let i = 0; i < text.length; i++) {
+				for (let key in letterScores) {
+					if (key.includes(text[i].toLowerCase())) {
+						wordScore *=
+							letterScores[key as keyof typeof letterScores];
+					}
+				}
+			}
+			setScore(score + wordScore);
+		}
+	};
+
+	return (
+		<div className="App">
+			<h1>Word Master</h1>
+			<WordGrid
+				board={board}
+				handleGridClick={handleGridClick}
+			></WordGrid>
+
+			<div className="Info">Score: {score}</div>
+
+			<div className="InputBox">
+				<button
+					className="btn btn-primary"
+					style={{
+						width: "8vh",
+						height: "6vh",
+						backgroundImage: `url(${send})`,
+						backgroundRepeat: "no-repeat",
+						backgroundPosition: "center",
+						backgroundSize: "35%",
+					}}
+					onClick={handleSubmitClick}
+				></button>
+				<input
+					style={{ width: "33.5vh", height: "6vh" }}
+					type="text"
+					value={text}
+					disabled
+				></input>
+				<button
+					className="btn btn-primary"
+					style={{
+						width: "8vh",
+						height: "6vh",
+						backgroundImage: `url(${erase})`,
+						backgroundRepeat: "no-repeat",
+						backgroundPosition: "center",
+						backgroundSize: "40%",
+					}}
+					onClick={handleEraseClick}
+				></button>
+			</div>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/WordGrid.tsx
@@ -0,0 +1,22 @@
+interface Props {
+	board: string[];
+	handleGridClick: (index: number) => void;
+}
+
+const WordGrid = ({ board, handleGridClick }: Props) => {
+	return (
+		<div className="board">
+			{board.map((item, index) => (
+				<div
+					key={index}
+					className="square disable-text-selection"
+					onClick={() => handleGridClick(index)}
+				>
+					{item}
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default WordGrid;
